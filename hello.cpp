a6f13f5f4f8b7b837650260d8a73cb237ef31c27
Youssef <myoussef9366@gmail.com>
2025-09-15 11:07:44 +0300

    feat: add I love you feature

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 using namespace std;
+
+void IloveYou(int x) {
+    if(x < 0) {
+        cout << "FUCK YOU BITCH" << endl;
+        return;
+    }
+    while(x--)
+        cout << "I love you <3" << endl;
+}
 int main(){
-    cout << "eqggfq";
+    cout << "eqaggfq";
+    IloveYou(1e6);
 }
