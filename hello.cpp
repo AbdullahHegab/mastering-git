1f486bcf64275def43fac499360372642a0cd8a0
Abdullah <abdullahhegab849@gmail.com>
2025-09-15 12:33:17 +0300

    message

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void IloveYou(int x) {
+    if(x < 0) {
+        cout << "HELLO" << endl;
+        return;
+    }
+    while(x--)
+        cout << "I love you ta3deelaaaa <333333" << endl;
+        cout << "Hegab";
+}
+void unusedfunction(int x) {
+    cout << "hii" << endl;
+}
+int main(){
+    IloveYou(1e6);
+    cout << "de7kkkk" << endl;
+}
