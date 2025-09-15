6e2aa28d74c5181d49657043b2419ce6066b7368
Youssef <myoussef9366@gmail.com>
2025-09-15 12:15:08 +0300

    fix: add unused function

--- hello.cpp
+++ hello.cpp
@@ -3,13 +3,17 @@ using namespace std;
 
 void IloveYou(int x) {
     if(x < 0) {
-        cout << "FUCK YYYsadffdsYSS BITCH" << endl;
+        cout << "FUCK YYYsadffdsYSS BITCH msh ba7eb 7egab" << endl;
         return;
     }
     while(x--)
         cout << "I love you <3" << endl;
 }
+void unusedfunction(int x) {
+    cout << "hii" << endl;
+}
 int main(){
-    cout << "eqaggfsasdffadsdfddssdq";
+    cout << "eqaggfsasdffadsdfddssdsdfadsfq" << endl;
+    cout << "ezayak ya bro" << endl;
     IloveYou(1e6);
 }
